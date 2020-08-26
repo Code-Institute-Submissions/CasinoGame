45b9e931743725f8837aa9259956169afe763e5e
tomod24 <thomo_d24@hotmail.co.uk>
2020-08-26 20:49:08 +0000

    Updated files and code. Fail screen not loading

--- script.js
+++ script.js
@@ -1,42 +1,166 @@
 class AudioController {
-    constructor() {
-        this.bgmusic = new Audio('Assets/Audio/music.mp3');
-        this.flipsound = new Audio('Assets/Audio/flip.mp3');
-        this.matchsound = new Audio('Assets/Audio/match.mp3');
-        this.winmusic = new Audio('Assets/Audio/win.mp3');
-        this.failmusic = new Audio('Assets/Audio/fail.mp3');
-        this.bgmusic.volume = 0.5;
-        this.bgmusic.loop = true;
+	constructor() {
+		this.bgmusic = new Audio('assets/Audio/music.mp3');
+		this.flipsound = new Audio('assets/Audio/flip.mp3');
+		this.matchsound = new Audio('assets/Audio/match.mp3');
+		this.winsound = new Audio('assets/Audio/win.mp3');
+		this.failsound = new Audio('Assets/Audio/fail.mp3');
+		this.bgmusic.volume = 0.5;
+		this.bgmusic.loop = true;
+	}
+	startMusic() {
+		this.bgmusic.play();
     }
-    startMusic() {
-        this.bgMusic.play();
+    
+	stopMusic() {
+		this.bgmusic.pause();
+		this.bgmusic.currentTime = 0;
     }
+    
+	flip() {
+		this.flipsound.play();
+    }
+    
+	match() {
+		this.matchsound.play();
+    }
+    
+	win() {
+		this.stopMusic();
+		this.winsound.play();
+    }
+    
+	fail() {
+		this.stopMusic();
+		this.failsound.play();
+	}
+}
+
+class casinogame {
+    constructor(totalTime, cards) {
+        this.cardsArray = cards;
+        this.totalTime = totalTime;
+        this.timeRemaining = totalTime;
+        this.timer = document.getElementById('time-remaining')
+        this.ticker = document.getElementById('flips');
+        this.audioController = new AudioController();
+    }
+
+  startGame() {
+        this.totalClicks = 0;
+        this.timeRemaining = this.totalTime;
+        this.cardToCheck = null;
+        this.matchedCards = [];
+        this.busy = true;
+        setTimeout(() => {
+            this.audioController.startMusic();
+            this.shuffleCards(this.cardsArray);
+            this.countdown = this.startCountdown();
+            this.busy = false;
+        }, 500)
+        this.hideCards();
+        this.timer.innerText = this.timeRemaining;
+        this.ticker.innerText = this.totalClicks;
+    }
+    startCountdown() {
+        return setInterval(() => {
+            this.timeRemaining--;
+            this.timer.innerText = this.timeRemaining;
+            if(this.timeRemaining === 0)
+                this.gameOver();
+        }, 1000);
+    }
+    gameOver() {
+        clearInterval(this.countdown);
+        this.audioController.gameOver();
+        document.getElementById('game-over-text').classList.add('visible');
+    }
+    win() {
+        clearInterval(this.countdown);
+        this.audioController.win();
+        document.getElementById('victory-text').classList.add('visible');
+    }
+    hideCards() {
+        this.cardsArray.forEach(card => {
+            card.classList.remove('visible');
+            card.classList.remove('matched');
+        });
+    }
+    flipCard(card) {
+        if(this.canFlipCard(card)) {
+            this.audioController.flip();
+            this.totalClicks++;
+            this.ticker.innerText = this.totalClicks;
+            card.classList.add('visible');
+
+            if(this.cardToCheck) {
+                this.checkForCardMatch(card);
+            } else {
+                this.cardToCheck = card;
+            }
+        }
+    }
+    checkForCardMatch(card) {
+        if(this.getCardType(card) === this.getCardType(this.cardToCheck))
+            this.cardMatch(card, this.cardToCheck);
+        else 
+            this.cardMismatch(card, this.cardToCheck);
+
+        this.cardToCheck = null;
+    }
+    cardMatch(card1, card2) {
+        this.matchedCards.push(card1);
+        this.matchedCards.push(card2);
+        card1.classList.add('matched');
+        card2.classList.add('matched');
+        this.audioController.match();
+        if(this.matchedCards.length === this.cardsArray.length)
+            this.win();
+    }
+    cardMismatch(card1, card2) {
+        this.busy = true;
+        setTimeout(() => {
+            card1.classList.remove('visible');
+            card2.classList.remove('visible');
+            this.busy = false;
+        }, 1000);
+    }
+    shuffleCards(cardsArray) { // Fisher-Yates Shuffle Algorithm.
+        for (let i = cardsArray.length - 1; i > 0; i--) {
+            let randIndex = Math.floor(Math.random() * (i + 1));
+            cardsArray[randIndex].style.order = i;
+            cardsArray[i].style.order = randIndex;
+        }
+    }
+    getCardType(card) {
+        return card.getElementsByClassName('card-value')[0].src;
+    }
+    canFlipCard(card) {
+        return !this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck;
+    }
+}
+
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready);
+} else {
+    ready();
 }
 
 function ready() {
     let overlays = Array.from(document.getElementsByClassName('overlay-text'));
     let cards = Array.from(document.getElementsByClassName('card'));
+    let game = new casinogame(5, cards);
 
     overlays.forEach(overlay => {
         overlay.addEventListener('click', () => {
             overlay.classList.remove('visible');
-            // game.startGame();
-            let audioController = new AudioController();
-             audioController.startMusic();
+            game.startGame();
         });
     });
+
     cards.forEach(card => {
         card.addEventListener('click', () => {
-            // game.flipCard(card);
+            game.flipCard(card);
         });
     });
-    
 }
-
-// When everything in the HTML file has loaded the then it will call the function provided.
-if(document.readyState === 'loading') {
-	document.addEventListener('DOMContentLoaded', ready());
-} else {
-	ready();
-}
-
