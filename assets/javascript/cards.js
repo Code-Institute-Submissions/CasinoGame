eb98335f84ff0ac1cc28d685365d5146e60386dd
tomod24 <thomo_d24@hotmail.co.uk>
2020-09-17 21:16:49 +0000

    Unique alt added for the dice images

--- assets/javascript/cards.js
+++ assets/javascript/cards.js
@@ -39,33 +39,33 @@ function buildCards() {
         document.getElementById('card_wrapper').insertAdjacentHTML('afterend',
             '<div class="card">'+
             '  <div class="card-back card-face">' +
-            '    <img class="dice dice-top-left" src="assets/images/dice.png" alt="card">'+
-            '    <img class="dice dice-top-right" src="assets/images/dice.png" alt="card">'+
-            '    <img class="dice dice-bottom-left" src="assets/images/dice.png" alt="card">'+
-            '    <img class="dice dice-bottom-right" src="assets/images/dice.png" alt="card">'+
+            '    <img class="dice dice-top-left" src="assets/images/dice.png" alt="card imagery dice">'+
+            '    <img class="dice dice-top-right" src="assets/images/dice.png" alt="card imagery dice">'+
+            '    <img class="dice dice-bottom-left" src="assets/images/dice.png" alt="card imagery dice">'+
+            '    <img class="dice dice-bottom-right" src="assets/images/dice.png" alt="card imagery dice">'+
             '    <img class="chip" src="assets/images/chip.png" alt="card">'+
             '  </div> ' +
             '  <div class="card-front card-face">' +
-            '     <img class="dice dice-top-left" src="assets/images/dice.png" alt="card">'+
-            '     <img class="dice dice-top-right" src="assets/images/dice.png" alt="card">'+
-            '     <img class="dice dice-bottom-left" src="assets/images/dice.png" alt="card">'+
-            '     <img class="dice dice-bottom-right" src="assets/images/dice.png" alt="card">'+
+            '     <img class="dice dice-top-left" src="assets/images/dice.png" alt="card imagery dice">'+
+            '     <img class="dice dice-top-right" src="assets/images/dice.png" alt="card imagery dice">'+
+            '     <img class="dice dice-bottom-left" src="assets/images/dice.png" alt="card imagery dice">'+
+            '     <img class="dice dice-bottom-right" src="assets/images/dice.png" alt="card imagery dice">'+
             '     <img class="win-card" src="assets/images/' +  card['image'] + '" alt="card">'+
             '  </div>' +
             '</div>' +
             '<div class="card">' +
             '   <div class="card-back card-face">' +
-            '      <img class="dice dice-top-left" src="assets/images/dice.png" alt="card">'+
-            '      <img class="dice dice-top-right" src="assets/images/dice.png" alt="card">'+
-            '      <img class="dice dice-bottom-left" src="assets/images/dice.png" alt="card">'+
-            '      <img class="dice dice-bottom-right" src="assets/images/dice.png" alt="card">'+
+            '      <img class="dice dice-top-left" src="assets/images/dice.png" alt="card imagery dice">'+
+            '      <img class="dice dice-top-right" src="assets/images/dice.png" alt="card imagery dice">'+
+            '      <img class="dice dice-bottom-left" src="assets/images/dice.png" alt="card imagery dice">'+
+            '      <img class="dice dice-bottom-right" src="assets/images/dice.png" alt="card imagery dice">'+
             '      <img class="chip" src="assets/images/chip.png" alt="card">'+
             '   </div>' +
             '   <div class="card-front card-face">' +
-            '      <img class="dice dice-top-left" src="assets/images/dice.png" alt="card">'+
-            '      <img class="dice dice-top-right" src="assets/images/dice.png" alt="card">'+
-            '      <img class="dice dice-bottom-left" src="assets/images/dice.png" alt="card">'+
-            '      <img class="dice dice-bottom-right" src="assets/images/dice.png" alt="card">'+
+            '      <img class="dice dice-top-left" src="assets/images/dice.png" alt="card imagery dice">'+
+            '      <img class="dice dice-top-right" src="assets/images/dice.png" alt="card imagery dice">'+
+            '      <img class="dice dice-bottom-left" src="assets/images/dice.png" alt="card imagery dice">'+
+            '      <img class="dice dice-bottom-right" src="assets/images/dice.png" alt="card imagery dice">'+
             '      <img class="win-card" src="assets/images/' +  card['image'] + '" alt="card">'+
             '   </div>' +
             '</div>'
